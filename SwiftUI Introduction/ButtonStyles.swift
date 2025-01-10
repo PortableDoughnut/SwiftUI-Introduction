9c5ff3d97ad751ffc5c4e7a9f09727b567fbd87a
PortableDoughnut <gwen.thelin@gmail.com>
2025-01-10 09:03:02 -0700

    The actual changes

---
+++ SwiftUI Introduction/ButtonStyles.swift
@@ -0,0 +1,52 @@
+//
+//  ButtonStyles.swift
+//  SwiftUI Introduction
+//
+//  Created by Gwen Thelin on 1/10/25.
+//
+
+import SwiftUI
+
+struct SquareButtonStyle: ViewModifier {
+	var toUseColour: Color
+	
+	init(isSelected: Bool = false) {
+		if isSelected {
+			toUseColour = Color.green
+		} else {
+			toUseColour = Color("NotSelectedButtonColour")
+		}
+	}
+	
+	func body(content: Content) -> some View {
+		content
+			.font(.headline)
+			.foregroundColor(.white)
+			.frame(width: 100, height: 100)
+			.padding()
+			.background(toUseColour)
+			.cornerRadius(18)
+	}
+}
+
+struct SubmitButtonStyle: ViewModifier {
+	var toUseColour: Color
+	
+	init(isSelected: Bool = false) {
+		switch isSelected {
+				case true:
+				toUseColour = Color.blue.opacity(0.5)
+			case false:
+				toUseColour = Color.blue
+		}
+	}
+	
+	func body(content: Content) -> some View {
+		content
+			.foregroundColor(.white)
+			.frame(width: 280, height: 50)
+			.background(toUseColour)
+			.cornerRadius(13)
+			.padding(.bottom, 61)
+	}
+}
--- SwiftUI Introduction/ContentView.swift
+++ SwiftUI Introduction/ContentView.swift
@@ -8,16 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State var selectedSport: Int = 0
+	@State var submitSelected: Bool = false
+	
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+			Spacer()
+			Text("Favorite Sport")
+				.font(.title)
+				.padding()
+			Spacer()
+			
+			HStack {
+				Button("Golf") {
+					selectedSport = 0
+				}.modifier(SquareButtonStyle(isSelected: selectedSport == 0))
+				
+				Button("Football") {
+					selectedSport = 1
+				}
+				.modifier(SquareButtonStyle(isSelected: selectedSport == 1))
+			}
+			
+			HStack {
+				Button("Soccer") {
+					selectedSport = 2
+				}
+				.modifier(SquareButtonStyle(isSelected: selectedSport == 2))
+				
+				Button("Baseball") {
+					selectedSport = 3
+				}
+				.modifier(SquareButtonStyle(isSelected: selectedSport == 3))
+			}
+			Spacer()
+			
+			Button("Submit") {
+				Task {
+					submitSelected = true
+					try await Task.sleep(for: .seconds(0.25))
+					submitSelected = false
+				}
+			}
+			.modifier(SubmitButtonStyle(isSelected: submitSelected))
         }
         .padding()
     }
 }
+
+
 
 #Preview {
     ContentView()
