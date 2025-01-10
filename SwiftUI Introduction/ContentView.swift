b88665435dae0032d8c3e19cc952d92bf4ebe808
PortableDoughnut <gwen.thelin@gmail.com>
2025-01-10 08:16:06 -0700

    Initial Commit

---
+++ SwiftUI Introduction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Introduction
+//
+//  Created by Gwen Thelin on 1/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Introduction/SwiftUI_IntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IntroductionApp.swift
+//  SwiftUI Introduction
+//
+//  Created by Gwen Thelin on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
